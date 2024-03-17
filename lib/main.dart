451821a333ad162263aca73f0bf7b8874a0937c7
parkyuhyeon <pyh1111802@naver.com>
2024-03-17 15:08:17 +0900

    🔥 :: 폴더와 파일을 spak 프로젝트에 맞게 변형

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:homework/pages/home/home_page.dart';
+import 'package:spak/pages/home/home_page.dart';
 
 void main() {
   runApp(const MyApp());
